db1c14d2bdac2a872914f144707fb620c6cb8c8a
sunggyupaik <melon9751@naver.com>
2024-10-11 05:40:57 +0900

    [fix] 장바구니 API를 수정하라
    장바구니 조회 시 장바구니 상품 정보들이 나오도록 수정합니다.

--- src/main/java/com/hhplus/commerce/domain/cart/CartItemRequest.java
+++ src/main/java/com/hhplus/commerce/domain/cart/CartItemRequest.java
@@ -15,6 +15,9 @@ public class CartItemRequest {
     @Schema(description = "상품 식별자", example = "1")
     private Long itemId;
 
+    @Schema(description = "상품 옵션 식별자", example = "2")
+    private Long itemOptionId;
+
     @Schema(description = "상품 갯수", example = "5")
     private Long quantity;
 }
