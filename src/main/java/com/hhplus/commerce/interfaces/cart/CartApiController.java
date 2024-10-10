0fa0d01272ce71954776ee1fb76267f86bfa1780
sunggyupaik <melon9751@naver.com>
2024-10-11 05:41:37 +0900

    [fix] 장바구니 API를 수정하라
    장바구니 조회 시 장바구니 상품 정보들이 나오도록 수정합니다.

--- src/main/java/com/hhplus/commerce/interfaces/cart/CartApiController.java
+++ src/main/java/com/hhplus/commerce/interfaces/cart/CartApiController.java
@@ -1,6 +1,7 @@
 package com.hhplus.commerce.interfaces.cart;
 
 import com.hhplus.commerce.common.response.CommonResponse;
+import com.hhplus.commerce.domain.cart.CartItemCommandResponse;
 import com.hhplus.commerce.domain.cart.CartItemRequest;
 import com.hhplus.commerce.domain.cart.CartItemResponse;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +14,41 @@ import java.util.List;
 public class CartApiController implements CartSpecification {
     @GetMapping("")
     public CommonResponse getCart(
-            @RequestHeader(value = "customerId", required = false) Long customerId
+            @RequestHeader(value = "customerId") Long customerId
     ) {
-        List<CartItemResponse> cartItemResponses = Arrays.asList(
-                new CartItemResponse(customerId, 2L, 3L),
-                new CartItemResponse(customerId, 3L, 5L),
-                new CartItemResponse(customerId, 4L, 10L)
-        );
+        CartItemResponse.CartItemOptionResponse cartItemOptionResponse1 = CartItemResponse.CartItemOptionResponse.builder()
+                .itemOptionId(1L)
+                .itemOptionSize("95")
+                .itemOptionColor("빨강")
+                .itemOptionPrice(0L)
+                .quantity(1L)
+                .build();
+
+        CartItemResponse cartItemResponse1 = CartItemResponse.builder()
+                .customerId(customerId)
+                .itemId(21L)
+                .itemName("겨울 코트")
+                .itemPrice(10000L)
+                .cartItemOptionResponse(cartItemOptionResponse1)
+                .build();
+
+        CartItemResponse.CartItemOptionResponse cartItemOptionResponse2 = CartItemResponse.CartItemOptionResponse.builder()
+                .itemOptionId(10L)
+                .itemOptionSize("105")
+                .itemOptionColor("파랑")
+                .itemOptionPrice(0L)
+                .quantity(12L)
+                .build();
+
+        CartItemResponse cartItemResponse2 = CartItemResponse.builder()
+                .customerId(customerId)
+                .itemId(22L)
+                .itemName("가을 난방")
+                .itemPrice(20000L)
+                .cartItemOptionResponse(cartItemOptionResponse2)
+                .build();
+
+        List<CartItemResponse> cartItemResponses = Arrays.asList(cartItemResponse1, cartItemResponse2);
 
         return CommonResponse.success(cartItemResponses);
     }
@@ -29,26 +58,28 @@ public class CartApiController implements CartSpecification {
             @RequestHeader("customerId") Long customerId,
             @RequestBody CartItemRequest request
     ) {
-        CartItemResponse cartItemResponse = CartItemResponse.builder()
+        CartItemCommandResponse cartItemCommandResponse = CartItemCommandResponse.builder()
                 .customerId(customerId)
                 .itemId(request.getItemId())
                 .quantity(request.getQuantity())
                 .build();
 
-        return CommonResponse.success(cartItemResponse);
+        return CommonResponse.success(cartItemCommandResponse);
     }
 
-    @DeleteMapping("/item/{itemId}")
+    @DeleteMapping("/item/{itemId}/item-option/{itemOptionId}")
     public CommonResponse deleteCart(
             @RequestHeader("customerId") Long customerId,
-            @PathVariable("itemId") Long itemId
+            @PathVariable("itemId") Long itemId,
+            @PathVariable("itemOptionId") Long itemOptionId
     ) {
-        CartItemResponse cartItemResponse = CartItemResponse.builder()
+        CartItemCommandResponse cartItemCommandResponse = CartItemCommandResponse.builder()
                 .customerId(customerId)
                 .itemId(itemId)
-                .quantity(3L)
+                .itemOptionId(itemOptionId)
+                .quantity(10L)
                 .build();
 
-        return CommonResponse.success(cartItemResponse);
+        return CommonResponse.success(cartItemCommandResponse);
     }
 }
