7d4e672b67f571db27ac65983049a3451ec3a73a
sunggyupaik <melon9751@naver.com>
2024-10-18 03:19:39 +0900

    [fix] 요청을 mockApi로 수정하라
    임시로 요청을 mockAPI로 수정합니다

--- src/main/java/com/hhplus/commerce/interfaces/item/ItemApiController.java
+++ src/main/java/com/hhplus/commerce/interfaces/item/ItemApiController.java
@@ -1,10 +1,12 @@
 package com.hhplus.commerce.interfaces.item;
 
 import com.hhplus.commerce.application.item.ItemQueryService;
-import com.hhplus.commerce.application.item.dto.ItemResponse;
 import com.hhplus.commerce.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,12 +18,14 @@ public class ItemApiController implements ItemApiSpecification {
     public CommonResponse getItem(
             @PathVariable("id") Long id
     ) {
-        ItemResponse itemResponse = itemQueryService.getItem(id);
-        return CommonResponse.success(itemResponse);
+//        ItemResponse itemResponse = itemQueryService.getItem(id);
+//        return CommonResponse.success(itemResponse);
+        return new ItemApiSpecification.Fake().getItem(id);
     }
     
     @GetMapping("/best")
     public CommonResponse getBestItems() {
+        //return new ItemApiSpecification.Fake().getBestItems();
         return new ItemApiSpecification.Fake().getBestItems();
     }
 }
--- src/main/java/com/hhplus/commerce/interfaces/point/PointApiController.java
+++ src/main/java/com/hhplus/commerce/interfaces/point/PointApiController.java
@@ -2,9 +2,8 @@ package com.hhplus.commerce.interfaces.point;
 
 import com.hhplus.commerce.application.point.PointChargeService;
 import com.hhplus.commerce.application.point.PointQueryService;
-import com.hhplus.commerce.common.response.CommonResponse;
-import com.hhplus.commerce.application.point.dto.PointResponse;
 import com.hhplus.commerce.application.point.dto.PointChargeRequest;
+import com.hhplus.commerce.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,9 +18,10 @@ public class PointApiController implements PointApiSpecification {
     public CommonResponse getPoint(
             @RequestHeader("customerId") Long customerId
     ) {
-        PointResponse pointResponse = pointQueryService.getPoint(customerId);
-
-        return CommonResponse.success(pointResponse);
+//        PointResponse pointResponse = pointQueryService.getPoint(customerId);
+//
+//        return CommonResponse.success(pointResponse);
+        return new PointApiSpecification.Fake().getPoint(customerId);
     }
 
     @PostMapping("/charge")
@@ -29,8 +29,9 @@ public class PointApiController implements PointApiSpecification {
             @RequestHeader("customerId") Long customerId,
             @RequestBody PointChargeRequest request
     ) {
-        Long charegdPoint = pointChargeService.chargePoint(customerId, request);
-
-        return CommonResponse.success(charegdPoint);
+//        Long charegdPoint = pointChargeService.chargePoint(customerId, request);
+//
+//        return CommonResponse.success(charegdPoint);
+        return new PointApiSpecification.Fake().chargePoint(customerId, request);
     }
 }
