f6d7e526b56cf9853f55a9b30327fa7ce90baacf
sunggyupaik <melon9751@naver.com>
2024-10-18 02:11:42 +0900

    [feat] 데이터 플랫폼 전송을 추가하라
    데이터 플랫폼에 send하는 간단한 메서들르 추가합니다

--- src/main/java/com/hhplus/commerce/application/order/OrderDataPlatformSendService.java
+++ src/main/java/com/hhplus/commerce/application/order/OrderDataPlatformSendService.java
@@ -2,9 +2,11 @@ package com.hhplus.commerce.application.order;
 
 import com.hhplus.commerce.domain.order.Order;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderDataPlatformSendService {
+    @Transactional
     public boolean send(Order order) {
         return true;
     }
