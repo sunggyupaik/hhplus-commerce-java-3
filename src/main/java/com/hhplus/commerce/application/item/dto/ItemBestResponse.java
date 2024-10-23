31b8d7793a716db0ef6e116a983715edf08e7020
sunggyupaik <melon9751@naver.com>
2024-10-23 21:15:17 +0900

    [test] 베스트 상품 조회를 테스트하라
    베스트 상품 저장소 조회를 테스트합니다. nativeQuery이므로 반환형을 인터페이스로 작성합니다

--- src/main/java/com/hhplus/commerce/application/item/dto/ItemBestResponse.java
+++ src/main/java/com/hhplus/commerce/application/item/dto/ItemBestResponse.java
@@ -1,20 +1,12 @@
 package com.hhplus.commerce.application.item.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Schema(description = "상위 상품 응답")
-public class ItemBestResponse {
+public interface ItemBestResponse {
     @Schema(description = "상품 식별자", example = "1")
-    private Long itemId;
+    Long getItemId();
 
-    @Schema(description = "상품 판매 수", example = "100")
-    private Long count;
+    @Schema(description = "상푼 판매 갯수", example = "10")
+    Long getCount();
 }
