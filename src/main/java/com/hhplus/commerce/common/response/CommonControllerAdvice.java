4e1418ea7cf1fef627f42c9f6ca1ec7e12d4a176
sunggyupaik <melon9751@naver.com>
2024-10-25 02:03:56 +0900

    [feat] 요청 전역 예외 처리를 추가하라
    요청으로 들어오는 파라미터의 예외처리를 추가합니다

--- src/main/java/com/hhplus/commerce/common/response/CommonControllerAdvice.java
+++ src/main/java/com/hhplus/commerce/common/response/CommonControllerAdvice.java
@@ -2,6 +2,9 @@ package com.hhplus.commerce.common.response;
 
 import com.hhplus.commerce.common.exception.BaseException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,5 +15,18 @@ public class CommonControllerAdvice {
     @ExceptionHandler(BaseException.class)
     public CommonResponse handleBaseException(BaseException e) {
         return CommonResponse.fail(e.getMessage(), e.getErrorCode().name());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public CommonResponse methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        FieldError fe = bindingResult.getFieldError();
+        if (fe != null) {
+            String message = "Request Error" + " " + fe.getField() + "=" + fe.getRejectedValue() + " (" + fe.getDefaultMessage() + ")";
+            return CommonResponse.fail(message, ErrorCode.COMMON_INVALID_PARAMETER.name());
+        } else {
+            return CommonResponse.fail(ErrorCode.COMMON_INVALID_PARAMETER.getErrorMsg(), ErrorCode.COMMON_INVALID_PARAMETER.name());
+        }
     }
 }
