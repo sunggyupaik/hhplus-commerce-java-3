d9f811f29393f98dbe52c6540fb94309d17a0cb8
sunggyupaik <melon9751@naver.com>
2024-10-25 02:22:59 +0900

    [feat] 예외 상황과 외부 통신에 로그를 추가하라
    예외 상황에 로그를 warn으로 외부 통신 실패에는 로그를 error로 추가합니다

--- src/main/java/com/hhplus/commerce/application/order/OrderDataPlatformSendService.java
+++ src/main/java/com/hhplus/commerce/application/order/OrderDataPlatformSendService.java
@@ -1,18 +1,20 @@
 package com.hhplus.commerce.application.order;
 
 import com.hhplus.commerce.domain.order.Order;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 public class OrderDataPlatformSendService {
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public boolean send(Order order) {
         try {
             return true;
         } catch (Exception e) {
-
+            log.error("cause = {}, errorMsg = {}", e, e.getMessage());
         }
 
         return false;
