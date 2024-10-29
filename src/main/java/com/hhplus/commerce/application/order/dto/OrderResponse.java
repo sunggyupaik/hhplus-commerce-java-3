9adfb4ad135a28f3159b1c4b6a12c9c452d9f19d
sunggyupaik <melon9751@naver.com>
2024-10-29 23:00:14 +0900

    [fix] @Setter를 제거하라
    안티패턴인 Setter를 제거합니ㄷ

--- src/main/java/com/hhplus/commerce/application/order/dto/OrderResponse.java
+++ src/main/java/com/hhplus/commerce/application/order/dto/OrderResponse.java
@@ -7,10 +7,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Builder
-@Setter
 @Getter
 @AllArgsConstructor
 @Schema(description = "주문 응답")
