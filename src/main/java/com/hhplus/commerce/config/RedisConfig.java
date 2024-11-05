b8e6d9c576ceb0d8aaec1f9c75395aa65fa2eee5
sunggyupaik <melon9751@naver.com>
2024-11-06 00:41:14 +0900

    [feat] 레디스 캐시 설정을 추가하라
    레디스 캐시 설정을 추가합니다

--- src/main/java/com/hhplus/commerce/config/RedisConfig.java
+++ src/main/java/com/hhplus/commerce/config/RedisConfig.java
@@ -1,29 +1,33 @@
 package com.hhplus.commerce.config;
 
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
-    private String redisHost;
+    private String host;
 
-    @Value(("${spring.data.redis.port}"))
-    private String redisPort;
-
-    private static final String REDISSON_HOST_PREFIX = "redis://";
+    @Value("${spring.data.redis.port}")
+    private int port;
 
     @Bean
-    public RedissonClient redissonClient() {
-        RedissonClient redisson = null;
-        Config config = new Config();
-        config.useSingleServer().setAddress("redis://localhost:6379");
-        //config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
-        redisson = Redisson.create(config);
-        return redisson;
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
     }
 }
