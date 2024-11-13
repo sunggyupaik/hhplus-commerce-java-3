bb10f4609d7d7bfbf9ac443c88bd146ccca857c3
sunggyupaik <melon9751@naver.com>
2024-11-13 22:33:54 +0900

    [refactor] 패키지 위치 이동
    일관성을 위해 패키지 위치를 변경합니다

--- src/main/java/com/hhplus/commerce/common/distributedLock/AopForTransactionNew.java
+++ src/main/java/com/hhplus/commerce/common/distributedLock/AopForTransactionNew.java
@@ -1,4 +1,4 @@
-package com.hhplus.commerce.common.aop;
+package com.hhplus.commerce.common.distributedLock;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Component;
