ef0e019fb19cc7065ad2dd54e0508a8cc7068909
sunggyupaik <melon9751@naver.com>
2024-11-22 00:21:05 +0900

    [feat] 카프카 설정을 추가하라
    데이터 플랫폼 전송을 위한 카프카 설정을 추가합니다

--- src/main/java/com/hhplus/commerce/config/KafkaConsumerConfig.java
+++ src/main/java/com/hhplus/commerce/config/KafkaConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.hhplus.commerce.config;
 
+import com.hhplus.commerce.application.order.dataPlatform.OrderDataPlatformEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -35,6 +36,23 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, Long> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, Long> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, OrderDataPlatformEvent> consumerOrderDataPlatformFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, host + ":" + port);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_2");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, OrderDataPlatformEvent> kafkaOrderDataPlatformListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, OrderDataPlatformEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerOrderDataPlatformFactory());
         return factory;
     }
 }
