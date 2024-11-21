868332d8725f6b07eb50bbd4b228da7696ea49ba
sunggyupaik <melon9751@naver.com>
2024-11-22 00:26:20 +0900

    [fix] 이벤트 리스너를 제거하라
    데이터플랫폼 전송의 이벤트 리스너를 제거합니다.

--- src/main/java/com/hhplus/commerce/application/order/dataPlatform/OrderDataPlatformListener.java
+++ src/main/java/com/hhplus/commerce/application/order/dataPlatform/OrderDataPlatformListener.java
@@ -3,20 +3,17 @@ package com.hhplus.commerce.application.order.dataPlatform;
 import com.hhplus.commerce.application.order.OrderDataPlatformManageService;
 import com.hhplus.commerce.domain.order.OrderCommand;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
 public class OrderDataPlatformListener {
     private final OrderDataPlatformManageService orderDataPlatformManageService;
 
-    @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+//    @Async
+//    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void orderDataPlatformHandler(OrderDataPlatformEvent event) {
         OrderDataPlatformPayload orderDataPlatformPayload = OrderDataPlatformPayload.of(event);
-        orderDataPlatformManageService.send(OrderCommand.OrderDataPlatformRequest.of(orderDataPlatformPayload.getOrderId()));
+        orderDataPlatformManageService.send(OrderCommand.OrderDataPlatformRequest.of(orderDataPlatformPayload));
     }
 }
