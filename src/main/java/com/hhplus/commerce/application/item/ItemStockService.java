dddb6ace94d52f94b42dbdf4e3f3afa9d98d5bf2
sunggyupaik <melon9751@naver.com>
2024-11-29 02:58:27 +0900

    [feat] Redis 주문 요청 재고 관리를 추가하라
    주문 요청한 상품의 재고 관리를 Redis로 하는 API를 추가합니다.

--- src/main/java/com/hhplus/commerce/application/item/ItemStockService.java
+++ src/main/java/com/hhplus/commerce/application/item/ItemStockService.java
@@ -7,7 +7,10 @@ import com.hhplus.commerce.domain.Item.itemInventory.ItemInventoryHistory;
 import com.hhplus.commerce.domain.order.Order;
 import com.hhplus.commerce.domain.order.item.OrderItem;
 import com.hhplus.commerce.support.distributedLock.DistributedLock;
+import com.hhplus.commerce.support.exception.IllegalStatusException;
+import com.hhplus.commerce.support.response.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
@@ -22,6 +25,23 @@ import java.util.List;
 public class ItemStockService {
     private final ItemReader itemReader;
     private final ItemStore itemStore;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Transactional
+    public Long decreaseStockRedis(Long itemOptionId, Long quantity, Long permitQuantity) {
+        String key = itemOptionId + ":stock";
+        String value = (redisTemplate.opsForValue().get(key));
+        Long orderedQuantity = value == null ? 0 : Long.parseLong(value);
+        if (orderedQuantity + quantity >= permitQuantity) {
+            throw new IllegalStatusException(ErrorCode.ITEM_STOCK_INSUFFICIENT);
+        }
+
+        redisTemplate.opsForValue().increment(key, quantity);
+
+        itemStore.createItemInventoryHistory(ItemInventoryHistory.createMinus(itemOptionId, quantity));
+
+        return permitQuantity - orderedQuantity;
+    }
 
     @Transactional
     public Long decreaseStockPessimistic(Long itemOptionId, Long quantity) {
--- src/main/java/com/hhplus/commerce/support/response/CommonControllerAdvice.java
+++ src/main/java/com/hhplus/commerce/support/response/CommonControllerAdvice.java
@@ -7,14 +7,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class CommonControllerAdvice {
-    @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = Exception.class)
     public CommonResponse onException(Exception e) {
