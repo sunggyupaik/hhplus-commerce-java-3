94ee3f6e50609128943643d178ed744cceb8a64a
sunggyupaik <melon9751@naver.com>
2024-11-29 02:53:58 +0900

    [feat] 상품 재고 이력을 추가하라
    상품 재고 증가/감소 시 이력을 추가합니다.

--- src/main/java/com/hhplus/commerce/application/item/ItemStockService.java
+++ src/main/java/com/hhplus/commerce/application/item/ItemStockService.java
@@ -1,10 +1,12 @@
 package com.hhplus.commerce.application.item;
 
-import com.hhplus.commerce.support.distributedLock.DistributedLock;
 import com.hhplus.commerce.domain.Item.ItemReader;
+import com.hhplus.commerce.domain.Item.ItemStore;
 import com.hhplus.commerce.domain.Item.itemInventory.ItemInventory;
+import com.hhplus.commerce.domain.Item.itemInventory.ItemInventoryHistory;
 import com.hhplus.commerce.domain.order.Order;
 import com.hhplus.commerce.domain.order.item.OrderItem;
+import com.hhplus.commerce.support.distributedLock.DistributedLock;
 import lombok.RequiredArgsConstructor;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.retry.annotation.Backoff;
@@ -19,11 +21,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ItemStockService {
     private final ItemReader itemReader;
+    private final ItemStore itemStore;
 
     @Transactional
     public Long decreaseStockPessimistic(Long itemOptionId, Long quantity) {
         ItemInventory itemInventory = itemReader.getItemInventoryWithPessimisticLock(itemOptionId);
         Long decreasedQuantity = itemInventory.decreaseStock(quantity);
+        itemStore.createItemInventoryHistory(ItemInventoryHistory.createMinus(itemOptionId, quantity));
 
         return decreasedQuantity;
     }
@@ -37,6 +41,7 @@ public class ItemStockService {
     public Long decreaseStockOptimisticLock(Long itemOptionId, Long quantity) {
         ItemInventory itemInventory = itemReader.getItemInventoryWithOptimisticLock(itemOptionId);
         Long decreasedQuantity = itemInventory.decreaseStock(quantity);
+        itemStore.createItemInventoryHistory(ItemInventoryHistory.createMinus(itemOptionId, quantity));
 
         return decreasedQuantity;
     }
@@ -45,6 +50,7 @@ public class ItemStockService {
     public Long decreaseStockDistributedLock(Long itemOptionId, Long quantity) {
         ItemInventory itemInventory = itemReader.getItemInventory(itemOptionId);
         Long decreasedQuantity = itemInventory.decreaseStock(quantity);
+        itemStore.createItemInventoryHistory(ItemInventoryHistory.createMinus(itemOptionId, quantity));
 
         return decreasedQuantity;
     }
@@ -53,6 +59,7 @@ public class ItemStockService {
     public Long increaseStock(Long itemOptionId, Long quantity) {
         ItemInventory itemInventory = itemReader.getItemInventoryWithPessimisticLock(itemOptionId);
         Long increasedQuantity = itemInventory.increaseStock(quantity);
+        itemStore.createItemInventoryHistory(ItemInventoryHistory.createPlus(itemOptionId, quantity));
 
         return increasedQuantity;
     }
@@ -67,6 +74,12 @@ public class ItemStockService {
             orderItems.forEach(orderItem -> {
                 ItemInventory itemInventory = itemReader.getItemInventoryWithPessimisticLock(orderItem.getItemId());
                 itemInventory.increaseStock(Long.valueOf(orderItem.getOrderCount()));
+                itemStore.createItemInventoryHistory(
+                        ItemInventoryHistory.createPlus(
+                                itemInventory.getItemOption().getId(),
+                                Long.valueOf(orderItem.getOrderCount())
+                        )
+                );
             });
         }
     }
